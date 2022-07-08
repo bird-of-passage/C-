cc02aec91dc7de395405fe505aa85edef4cf4b5c
Invincible-21 <shantobd1969@gmail.com>
2022-07-09 01:36:29 +0600

    work hard

---
+++ Lect-2.2/Eid_salami.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ammount1, ammount2;
+    cin >> ammount1 >> ammount2;
+    int sum = ammount1 + ammount2;
+    cout << sum << endl;
+    cout << "sum\n";
+
+    return 0;
+}
