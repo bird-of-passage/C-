259b3895a3311f1a0ef59bbbf9ce87fbd6c28f56
Invincible-21 <shantobd1969@gmail.com>
2022-07-20 12:37:36 +0600

    ilfe goes on

--- Lect-4.2_Advanced_Pattern.cpp/Rhombus_Pattern.cpp
+++ Lect-4.2_Advanced_Pattern.cpp/Rhombus_Pattern.cpp
@@ -8,9 +8,13 @@ int main()
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n + 1 - i; j++)
+        for (int j = 1; j <= n - i; j++)
         {
-            cout << j << " ";
+            cout << " ";
+        }
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "* ";
         }
         cout << endl;
     }
