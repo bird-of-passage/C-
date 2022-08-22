a4fec46c3857b2d5176da00013d26ffe38e3d929
Invincible-21 <shantobd1969@gmail.com>
2022-08-23 00:08:06 +0600

    programs added

--- Lect-6.2_Functions/nCr.cpp
+++ Lect-6.2_Functions/nCr.cpp
@@ -4,3 +4,27 @@ nCr = --------------
       (n-r)! x r!
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n)
+{
+    int factorial = 1;
+    for(int i=2; i<=n; i++)
+    {
+        factorial*=i;
+    }
+    return factorial;
+}
+
+int main()
+{
+    int n,r;
+    cin>>n>>r;
+
+    int ans = fact(n)/(fact(n-r)*fact(r));
+    cout<<ans<<endl;
+
+    return 0;
+}
