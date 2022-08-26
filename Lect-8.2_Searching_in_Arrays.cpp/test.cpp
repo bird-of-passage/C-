261a5ed041c06b6e7ac7bd02c64eb189c3a7c594
Invincible-21 <shantobd1969@gmail.com>
2022-08-26 21:08:52 +0600

    work hard

--- Lect-8.2_Searching_in_Arrays.cpp/test.cpp
+++ Lect-8.2_Searching_in_Arrays.cpp/test.cpp
@@ -1,15 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int linearSearch(int arr[],int n, int key){
-    for(int i=0; i<n; i++){
-        if(arr[i]==key){
-            return i;
-        }
-    }
-    return -1;
-}
+int binarySearch(int arr[], int n, int key){
+    int s=0;
+    int e=n;
 
+ while (s<=e){
+    
+    int mid = (s+e)/2;
+
+    if(arr[mid]==key){
+        return mid;
+    }
+    else if(arr[mid]>key){
+        e=mid-1;
+    }
+    else{
+        s=mid+1;
+    }  
+ }
+return -1;
+}
 
 int main(){
 
@@ -24,7 +35,7 @@ int main(){
     int key;
     cin>>key;
 
-    cout<<linearSearch(arr,n,key)<<endl;
+    cout<<binarySearch(arr,n,key)<<endl;
 
     return 0;
 }
