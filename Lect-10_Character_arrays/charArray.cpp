061c57eeba8beeb33d74cdcccad5539e4a5a5bdc
Invincible-21 <shantobd1969@gmail.com>
2022-09-13 19:07:40 +0600

    happy programmers day!

---
+++ Lect-10_Character_arrays/charArray.cpp
@@ -0,0 +1,21 @@
+// What is character arrays?
+// --> Character arrays are arrays of characters
+
+// #Declaration:
+
+// char arr[n+1]
+// "rahul/0"  ----> '\0' NULL character
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    char arr[100];
+    cin>>arr;
+
+    cout<<arr[2];
+    return 0;
+}
